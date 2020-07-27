1ce3895810db51197ed80407951c5a2ebef014ee
cmjnorman <cmjnorman@gmail.com>
2020-07-27 11:21:21 +0100

    Add database model and initial migration

---
+++ Decibase_DbLayer/Decibase_DbLayer/Model.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace Decibase_DbLayer
+{
+    public class DecibaseContext : DbContext
+    {
+        public DbSet<Track> Tracks { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=Decibase;");
+
+    }
+
+    public class Track
+    {
+        public int TrackId { get; set; }
+        public string Title { get; set; }
+        public int TrackNumber { get; set; }
+        public int DiscNumber { get; set; }
+        public string Genre { get; set; }
+
+        public int AlbumId { get; set; }
+        public Album Album { get; set; }
+        
+        public virtual ICollection<Artist> Features { get; } = new List<Artist>();
+        public virtual ICollection<Artist> Artists { get; } = new List<Artist>();
+    }
+
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Name { get; set; }
+        public int TotalTracks { get; set; }
+        public int TotalDiscs { get; set; }
+        public string Year { get; set; }
+        public virtual ICollection<Track> Tracks { get; } = new List<Track>();
+    }
+
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+        public string Nationality { get; set; }
+        public virtual ICollection<Track> Tracks { get; } = new List<Track>();
+    }
+}
---
+++ Decibase_DbLayer/Decibase_DbLayer/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Decibase_DbLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using(var db = new BloggingContext())
+            //{
+            //    //db.Add(new Track { Title = "Song 1" });
+            //    //db.Add(new Artist { Name = "Artist 1" });
+            //    //var track = db.Tracks.Where(t => t.Title == "Song 1").Include(t => t.Artists).FirstOrDefault();
+            //    //var artist = db.Artists.Where(a => a.Name == "Artist 1").Include(a => a.Tracks).FirstOrDefault();
+            //    //track.Artists.Add(artist);
+            //    //artist.Tracks.Add(track);
+            //}
+
+        }
+    }
+}
