ee681c8dd51b1c7dce4ffff5dea046a988094b63
cmjnorman <cmjnorman@gmail.com>
2020-07-28 10:15:45 +0100

    Rename solution, add track properties 'Features' and 'Remixers', and add ERD for database

---
+++ Decibase/Decibase_DbLayer/Model Classes/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decibase_DbLayer
+{
+    public partial class Artist
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+        public string Nationality { get; set; }
+        public List<TrackArtist_JunctionTable> Tracks { get; set; }
+    }
+}
---
+++ Decibase/Decibase_DbLayer/Model Classes/Track.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decibase_DbLayer
+{ 
+    public partial class Track
+    {
+        public int TrackId { get; set; }
+        public string Title { get; set; }
+        public int TrackNumber { get; set; }
+        public int DiscNumber { get; set; }
+        public string Genre { get; set; }
+
+        public int AlbumId { get; set; }
+        public Album Album { get; set; }
+
+        public List<TrackArtist_JunctionTable> Artists { get; set; }
+        public List<TrackArtist_JunctionTable> Features { get; set; }
+        public List<TrackArtist_JunctionTable> Remixers { get; set; }
+    }
+}
