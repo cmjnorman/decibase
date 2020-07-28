81ef8193572111afd5d842a2eb952f319f5fb401
cmjnorman <cmjnorman@gmail.com>
2020-07-28 14:03:04 +0100

    Add class customisations to override Equals and GetHashCode methods for each model class, and add tests to test these new methods

---
+++ Decibase/Decibase_Model/Model Classes/Class Customisations/TrackArtist_JunctionCust.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decibase_Model
+{
+    public partial class TrackArtist_Junction
+    {
+        public override string ToString()
+        {
+            return $"{Track.Title}-{Artist.Name} Junction";
+        }
+    }
+}
